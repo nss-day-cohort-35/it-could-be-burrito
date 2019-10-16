df929746c61eb902fc9185fa08345dd3cd411fd4
Brenda Long <brenda@studio6thave.com>
2019-10-16 09:11:22 -0500

    adding login with user flow

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ApplicationViews from './components/ApplicationViews';
+import Nav from './components/nav/Nav';
+import Login from './components/auth/Login'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: localStorage.getItem("credentials") !== null
+  }
+
+  // Check if credentials are in local storage
+  //returns true/false
+  isAuthenticated = () => localStorage.getItem("credentials") !== null
+
+
+  setUser = (authObj) => {
+    /*
+      For now, just store the email and password that
+      the customer enters into local storage.
+    */
+    localStorage.setItem(
+      "credentials",
+      JSON.stringify(authObj)
+    )
+    this.setState({
+      user: this.isAuthenticated()
+    });
+  }
+
+  render() {
+    return(
+      <>
+        {this.state.user ?
+          <>
+            <Nav />
+            <ApplicationViews />
+          </>
+          :
+          <Login setUser={this.setUser} />
+        }
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ApplicationViews extends Component {
+
+  render() {
+    return <h2>ApplicationViews</h2>
+  }
+}
+
+export default ApplicationViews;
